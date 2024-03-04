2c0999351b56a4bbec67f543f3af6a75ba348906
ChinaiArman <chinaiarman@gmail.com>
2024-03-04 13:26:22 -0800

    Create file structure

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+
+app.use(express.static('public'))
+app.use('/css', express.static('public/css'))
+app.use('/js', express.static('public/js'))
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+const PORT = process.env.PORT || 3000;
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.listen(PORT, () => console.info(`App listening on port ${PORT}`))
