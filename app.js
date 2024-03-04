dc1ddf6ecffabcbbd1eb5fa89749c5ba72a6d9b0
ChinaiArman <chinaiarman@gmail.com>
2024-03-04 15:39:01 -0800

    Demo pass in variables from server

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ app.set('view engine', 'ejs')
 const PORT = process.env.PORT || 3000;
 
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', { linkResult: 'variable' })
+})
+
+app.get('/temp', (req, res) => {
+    let linkResult = req.originalUrl.replace("/temp?key=", "").replace("%20", " ");
+    res.render('index', { linkResult: linkResult })
 })
 
 app.listen(PORT, () => console.info(`App listening on port ${PORT}`))
