384f7fdce86ad74f1d1bd92b95adfb03129236cc
Natalie Yu <nyu17@my.bcit.ca>
2024-03-09 23:04:40 -0800

    Add login form

---
+++ public/scripts/login.js
@@ -0,0 +1,5 @@
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log(`logging in with user: ${username} and password: ${password}`);
+}
