727ce43839ca2dd39183f476c27f2a3157a63be6
ChinaiArman <chinaiarman@gmail.com>
2024-03-12 03:17:57 -0700

    Remove old node/ejs project

--- old/public/scripts/index.js
+++
@@ -1,5 +0,0 @@
-console.log("Script connected...")
-
-let callServer = function () {
-    window.location.assign("/changeGreeting?greeting=" + document.getElementById("inputBox").value)
-}
--- old/public/scripts/login.js
+++
@@ -1,5 +0,0 @@
-function login() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    console.log(`logging in with user: ${username} and password: ${password}`);
-}
