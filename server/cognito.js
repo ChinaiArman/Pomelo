9ff0f0f28c81263d5745378281a93b2d9983c920
ChinaiArman <chinaiarman@gmail.com>
2024-03-12 03:18:11 -0700

    Create cognito.js boilerplate

---
+++ server/cognito.js
@@ -0,0 +1,36 @@
+import { CognitoUserPool, AuthenticationDetails, CognitoUser } from "amazon-cognito-identity-js";
+
+const poolData = {
+    UserPoolId: "",
+    ClientId: "",
+    region: "",
+};
+const userPool = new CognitoUserPool(poolData);
+
+function authenticateUserLogin(username, password) {
+    const authenticateData = {
+        Username: username,
+        Password: password,
+    };
+    const authenticationDetails = new AuthenticationDetails(authenticateData);
+
+    const userData = {
+        Username: username,
+        Pool: userPool,
+    };
+    const cognitoUser = new CognitoUser(userData);
+
+    return new Promise((resolve, reject) => {
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: function (result) {
+                const accessToken = result.getAccessToken().getJwtToken();
+                resolve(accessToken);
+                // TODO: redirect to logged-in pages
+            },
+            onFailure: function (err) {
+                reject(err);
+            },
+        });
+    });
+}
+  
