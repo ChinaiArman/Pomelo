d92caa9f6e5a3803b518fa8ae3a2e8900e73e263
ChinaiArman <chinaiarman@gmail.com>
2024-03-14 02:04:08 -0700

    Add first post route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 require('dotenv').config()
+const { createNewTeamSpace } = require('./dynamo.js');
+
 
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
 
 
 const PORT = process.env.PORT || 5000;
@@ -11,9 +20,9 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/api', (req, res) => {
-    res.json({"users": ["user1", "user2", "user3"]})
+app.post("/createTeamSpace", (req, res) => {
+  createNewTeamSpace(req.body.teamSpaceName, req.body.teamSpaceLeaderUserID, req.body.teamSpaceUserName);
+  res.send("Success");
 });
-
 
 app.listen(PORT, () => console.info(`App listening on port ${PORT}`))
