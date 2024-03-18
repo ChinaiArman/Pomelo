26e9bdb2f717f3ad56f164ec319763955bc008de
ChinaiArman <chinaiarman@gmail.com>
2024-03-18 14:12:46 -0700

    Create login redirect functionality.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,25 +11,28 @@ import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 
+var loggedIn = window.localStorage.getItem("userID")
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<Home />
+    element: <Home />,
+    loader: () => (!loggedIn ? window.location.href = '/login' : null),
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
+    loader: () => (loggedIn ? window.location.href = '/' : null),
   },
 ])
 
 function App() {
   return (
-      <div className="App">
-        <NavBar />
-        <RouterProvider router={router} />
-        <Footer />
-      </div>
+    <div className="App">
+      <NavBar />
+      <RouterProvider router={router} />
+      <Footer />
+    </div>
   )
 }
 
