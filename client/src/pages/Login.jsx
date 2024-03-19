b79c39a8b6bf1d0ffaa4d115169427d43d91b916
Natalie Yu <nyu17@my.bcit.ca>
2024-03-19 15:30:49 -0700

    Clear username/password input fields on invalid login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -16,6 +16,8 @@ const Login = () => {
                 await getTeamSpaceID(response.data.data.userID)
             } else {
                 setErrorMessage('Invalid username or password. Please try again.');
+                setUsername('');
+                setPassword('');
             }
         })
         .catch(error => { console.log(error) });
@@ -76,6 +78,7 @@ let getTeamSpaceID = async function (userID) {
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Enter your username"
                     required
+                    value={username}
                     onChange={(e) => setUsername(e.target.value)}
                   />
                 </div>
@@ -93,6 +96,7 @@ let getTeamSpaceID = async function (userID) {
                     placeholder="Enter your password"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
+                    value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
