d6585af24391692f669a7f2947916d2d2bf08c06
Natalie Yu <nyu17@my.bcit.ca>
2024-03-20 13:03:43 -0700

    Redirect transaction items to their category page

--- client/src/components/TransactionsTable.jsx
+++ client/src/components/TransactionsTable.jsx
@@ -2,30 +2,37 @@ import { Table } from "flowbite-react";
 
 const TransactionsTable = ({ transactions}) => {
     return (
-        <div className="mt-5 mb-5" style={{zIndex: -1}}>
-        <Table hoverable>
-            <Table.Head>
-            <Table.HeadCell>Item</Table.HeadCell>
-            <Table.HeadCell>Amount</Table.HeadCell>
-            <Table.HeadCell>Spending Category</Table.HeadCell>
-            <Table.HeadCell>User</Table.HeadCell>
-            <Table.HeadCell>Date</Table.HeadCell>
-            </Table.Head>
-            <Table.Body className="divide-y">
-            {transactions.map((transaction, index) => (
-                <Table.Row
-                key={index}
-                className="bg-white dark:border-gray-700 dark:bg-gray-800"
-                >
-                <Table.Cell>{transaction.transactionName}</Table.Cell>
-                <Table.Cell>{transaction.transactionAmount}</Table.Cell>
-                <Table.Cell>{transaction.spendingCategoryName}</Table.Cell>
-                <Table.Cell>{transaction.username}</Table.Cell>
-                <Table.Cell>{transaction.transactionDate} </Table.Cell>
-                </Table.Row>
-            ))}
-            </Table.Body>
-        </Table>
+        <div className="mt-5 mb-5">
+            <Table hoverable style={{zIndex: -1}}>
+                <Table.Head>
+                <Table.HeadCell>Item</Table.HeadCell>
+                <Table.HeadCell>Amount</Table.HeadCell>
+                <Table.HeadCell>Spending Category</Table.HeadCell>
+                <Table.HeadCell>User</Table.HeadCell>
+                <Table.HeadCell>Date</Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                {transactions.map((transaction, index) => {
+                    const categoryId = transaction.spendingCategoryID
+                    var redirect = "/categories?id=" + categoryId;
+                    return (
+                        <Table.Row
+                            key={index}
+                            className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                            onClick={() => window.location.replace(redirect)}
+                            style={{ cursor: "pointer" }}
+                        >
+                            <Table.Cell>{transaction.transactionName}</Table.Cell>
+                            <Table.Cell>{transaction.transactionAmount}</Table.Cell>
+                            <Table.Cell>{transaction.spendingCategoryName}</Table.Cell>
+                            <Table.Cell>{transaction.username}</Table.Cell>
+                            <Table.Cell>{transaction.transactionDate}</Table.Cell>
+                        </Table.Row>
+                    )
+                        
+                })}
+                </Table.Body>
+            </Table>
         </div>
     );
 };
