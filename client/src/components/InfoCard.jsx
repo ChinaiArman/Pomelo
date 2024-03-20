10db17aa9a7af52e1f971c1cbb7047549f3de989
Natalie Yu <nyu17@my.bcit.ca>
2024-03-19 21:50:41 -0700

    Add currencyFormatter to format dollar amounts

--- client/src/components/InfoCard.jsx
+++ client/src/components/InfoCard.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { currencyFormatter } from "../utils";
 
 const InfoCard = ({ title, value }) => {
   
     return (
-    <div className="flex justify-center items-center bg-gray-200 p-4 rounded-md m-2">
-      <div>
-        <h3 className="text-lg font-semibold">{title}</h3>
-        <p className="text-xl">{value}</p>
+      <div className="flex justify-center items-center bg-gray-200 p-4 rounded-md m-2">
+        <div>
+          <h3 className="text-lg font-semibold">{title}</h3>
+          <p className="text-xl">{currencyFormatter.format(value)}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default InfoCard;
