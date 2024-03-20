e86990d48e8e19cabeaa24d568a435a48f8999e5
ChinaiArman <chinaiarman@gmail.com>
2024-03-19 20:41:17 -0700

    Add boilerplate

---
+++ client/src/pages/Transactions.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Transactions = () => {
+
+    return (
+        <div className="transactions">
+            <p>Transactions</p>
+        </div>
+    );
+}
+
+export default Transactions;
