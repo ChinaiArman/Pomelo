5638c50624e8f123a0d70ea59c9c4b3ae64c0c83
Natalie Yu <nyu17@my.bcit.ca>
2024-03-20 01:40:37 -0700

    Fix TransactionsTable persisting with popup

--- client/src/components/TransactionsTable.jsx
+++ client/src/components/TransactionsTable.jsx
@@ -1,33 +1,33 @@
 import { Table } from "flowbite-react";
 
-const TransactionsTable = ({ transactions }) => {
-  return (
-    <div>
-      <Table hoverable>
-        <Table.Head>
-          <Table.HeadCell>Item</Table.HeadCell>
-          <Table.HeadCell>Amount</Table.HeadCell>
-          <Table.HeadCell>Spending Category</Table.HeadCell>
-          <Table.HeadCell>User</Table.HeadCell>
-          <Table.HeadCell>Date</Table.HeadCell>
-        </Table.Head>
-        <Table.Body className="divide-y">
-          {transactions.map((transaction, index) => (
-            <Table.Row
-              key={index}
-              className="bg-white dark:border-gray-700 dark:bg-gray-800"
-            >
-              <Table.Cell>{transaction.transactionName}</Table.Cell>
-              <Table.Cell>{transaction.transactionAmount}</Table.Cell>
-              <Table.Cell>{transaction.spendingCategoryName}</Table.Cell>
-              <Table.Cell>{transaction.username}</Table.Cell>
-              <Table.Cell>{transaction.transactionDate} </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    </div>
-  );
+const TransactionsTable = ({ transactions}) => {
+    return (
+        <div style={{zIndex: -1}}>
+        <Table hoverable>
+            <Table.Head>
+            <Table.HeadCell>Item</Table.HeadCell>
+            <Table.HeadCell>Amount</Table.HeadCell>
+            <Table.HeadCell>Spending Category</Table.HeadCell>
+            <Table.HeadCell>User</Table.HeadCell>
+            <Table.HeadCell>Date</Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+            {transactions.map((transaction, index) => (
+                <Table.Row
+                key={index}
+                className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                <Table.Cell>{transaction.transactionName}</Table.Cell>
+                <Table.Cell>{transaction.transactionAmount}</Table.Cell>
+                <Table.Cell>{transaction.spendingCategoryName}</Table.Cell>
+                <Table.Cell>{transaction.username}</Table.Cell>
+                <Table.Cell>{transaction.transactionDate} </Table.Cell>
+                </Table.Row>
+            ))}
+            </Table.Body>
+        </Table>
+        </div>
+    );
 };
 
 export default TransactionsTable;
