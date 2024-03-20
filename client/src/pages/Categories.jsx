3b4f74bb3df54fb7c4d1c50774f7d4cc937bde45
ChinaiArman <chinaiarman@gmail.com>
2024-03-19 20:41:24 -0700

    Add boilerplate

---
+++ client/src/pages/Categories.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Categories = () => {
+
+    return (
+        <div className="categories">
+            <p>Categories</p>
+        </div>
+    );
+}
+
+export default Categories;
