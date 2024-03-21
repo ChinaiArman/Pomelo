be8dcf2c68b900add0b9fc36ea89829332531426
ChinaiArman <chinaiarman@gmail.com>
2024-03-21 11:45:44 -0700

    Add categories link

--- client/src/components/Jumbotron.jsx
+++ client/src/components/Jumbotron.jsx
@@ -44,7 +44,7 @@ const Jumbotron = ({ username, transactions, spendingCategory, teamSpaceName }) 
                         <div className="flex flex-col items-center justify-center">
                             <CategoryCard key="largest-category" category={spendingCategory} />
                         </div>
-                        <a href="#" className="mt-3 text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">See all spending categories
+                        <a href="#categories" className="mt-3 text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">See all spending categories
                             <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
