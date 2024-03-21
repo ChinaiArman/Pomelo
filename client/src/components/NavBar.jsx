8df5f362b1ccd3e14d8ffceb978021bb07310f4f
ChinaiArman <chinaiarman@gmail.com>
2024-03-21 10:37:06 -0700

    Update nav bar design

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -24,50 +24,52 @@ const NavBar = () => {
   }
 
   const handleLogout = () => {
-      window.localStorage.removeItem("userID")
-      window.localStorage.removeItem("username");
-      window.localStorage.removeItem("teamSpaceID");
-      window.location.replace('/login')
+    window.localStorage.removeItem("userID")
+    window.localStorage.removeItem("username");
+    window.localStorage.removeItem("teamSpaceID");
+    window.location.replace('/login')
   }
 
   const navItems = [
     { id: 1, text: "Home", link: "/" },
-    { id: 2, text: "Categories", link: "/categories" },
     { id: 3, text: "Transactions", link: "/transactions" },
     { id: 4, text: "Settings", link: "/signup" },
   ];
 
   return (
-    <div className="bg-gray-100 flex justify-between items-center h-24 w-full mx-auto px-4 text-gray-800">
-      <ul className="flex">
-        {navItems.slice(0, -1).map((item) => {
-          if (item.link === window.location.pathname) {
-            return (
-              <li
-                key={item.id}
-                className="p-4 hover:bg-gray-300 rounded-xl m-2 cursor-pointer duration-300 bg-gray-300"
-              >
-                <a href={item.link}>{item.text}</a>
-              </li>
-            )
-          } else {
-          return (
-          <li
-            key={item.id}
-            className="p-4 hover:bg-gray-300 rounded-xl m-2 cursor-pointer duration-300 hover:text-black"
-          >
-            <a href={item.link}>{item.text}</a>
-          </li>
-        )}
-        })}
-      </ul>
-
-      <Dropdown label="Settings" inline>
-        <Dropdown.Item onClick={() => handleLogout()}>Sign out</Dropdown.Item>
-        {/* if is teamspace leader, show team space settings tab */}
-        {isLeader && <Dropdown.Item href="/teamSpaceSettings">Team Space Settings</Dropdown.Item>}
-      </Dropdown>
-    </div>
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" className="h-8" alt="Flowbite Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MyApp</span>
+        </a>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            {navItems.slice(0, -1).map((item) => {
+              if (item.link === window.location.pathname) {
+                return (
+                  <li>
+                    <a href={item.link} className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">{item.text}</a>
+                  </li>
+                )
+              } else {
+                return (
+                  <li>
+                    <a href={item.link} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{item.text}</a>
+                  </li>
+                )
+              }
+            })}
+            <li>
+              <Dropdown label="Settings" inline>
+                <Dropdown.Item onClick={() => handleLogout()}>Sign out</Dropdown.Item>
+                {isLeader && <Dropdown.Item href="/teamSpaceSettings">Team Space Settings</Dropdown.Item>}
+              </Dropdown>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
