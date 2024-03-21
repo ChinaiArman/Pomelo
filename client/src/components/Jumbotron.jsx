5439fcb44c5de21c3b7de262205fcf8c704b5aff
ChinaiArman <chinaiarman@gmail.com>
2024-03-21 11:28:04 -0700

    Create jumbotron

---
+++ client/src/components/Jumbotron.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { currencyFormatter } from "../utils";
+
+import LatestTransactionsCard from "./LatestTransactionsCard";
+import CategoryCard from "./CategoryCard";
+
+const Jumbotron = ({ username, transactions, spendingCategory, teamSpaceName }) => {
+    return (
+        <section className="bg-white dark:bg-gray-900">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
+                <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+                    <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">Hello {username}!</h1>
+                    <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">Welcome to <b>{teamSpaceName}</b>. Below is a summary of your latest transactions and your spending categories</p>
+                    <a href="#categories" className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                        See spending categories
+                        <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a>
+                </div>
+                <div className="grid md:grid-cols-2 gap-8 justify-center">
+                    <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 justify-center items-center">
+                        <a href="#" className="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
+                            <svg className="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                                <path d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
+                            </svg>
+                            Transactions
+                        </a>
+                        <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Latest Transactions</h2>
+                        <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Here is a look at the most recent transactions from the last 24 hours.</p>
+                        <div className="flex flex-col items-center justify-center">
+                            <LatestTransactionsCard transactions={transactions} />
+                        </div>
+                    </div>
+                    <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                        <a href="#categories" className="bg-purple-100 text-purple-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-purple-400 mb-2">
+                            <svg className="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15" />
+                            </svg>
+                            Spending Categories
+                        </a>
+                        <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Largest Spending Category</h2>
+                        <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Here is a look at your team space's largest spending category.</p>
+                        <div className="flex flex-col items-center justify-center">
+                            <CategoryCard key="largest-category" category={spendingCategory} />
+                        </div>
+                        <a href="#" className="mt-3 text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">See all spending categories
+                            <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Jumbotron;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,138 +4,119 @@ import { VscDiffAdded } from "react-icons/vsc";
 
 
 import InfoCard from '../components/InfoCard';
+import Jumbotron from '../components/Jumbotron';
 import CategoryCard from '../components/CategoryCard';
 import CreateCategoryModal from "../components/CreateCategoryModal";
 import CreateTransactionModal from '../components/CreateTransactionModal';
 import LatestTransactionsCard from '../components/LatestTransactionsCard';
 
 const Home = () => {
-    const [teamSpaceName, setTeamSpaceName] = useState('')
-    const [totalSpent, setTotalSpent] = useState('');
-    const [totalBudget, setTotalBudget] = useState('');
-    const [spendingCategories, setSpendingCategories] = useState([]);
-    const [transactions, setTransactions] = useState([]);
+  const [teamSpaceName, setTeamSpaceName] = useState('')
+  const [totalSpent, setTotalSpent] = useState('');
+  const [totalBudget, setTotalBudget] = useState('');
+  const [spendingCategories, setSpendingCategories] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  const [largestSpendingCategory, setLargestSpendingCategory] = useState('');
 
-    const [isCreateCategoryModalOpen, setIsCreateCategoryModalOpen] = useState(false);
-    const [isAddTransactionModalOpen, setIsAddTransactionModalOpen] = useState(false);
+  const [isCreateCategoryModalOpen, setIsCreateCategoryModalOpen] = useState(false);
 
-    const [isLeader, setIsLeader] = useState(false);
+  const [isLeader, setIsLeader] = useState(false);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        await axios.get('http://localhost:5000/getTeamSpaceTotalBudget', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
-            .then(response => {
-                setTotalBudget(response.data.data.totalBudget);
-            }).catch(error => {
-                console.log(error);
-            });
-        await axios.get('http://localhost:5000/getTeamSpaceTotalAmountUsed', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
-            .then(response => {
-                setTotalSpent(response.data.data.totalAmountUsed);
-            }).catch(error => {
-                console.log(error);
-            });
-        await axios.get('http://localhost:5000/getAllSpendingCategories', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
-            .then(response => {
-                setSpendingCategories(response.data.data);
-            }).catch(error => {
-                console.log(error);
-            });
-        await axios.get('http://localhost:5000/getRecentTransactions', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
-            .then(response => {
-                setTransactions(response.data.data.recentTransactions);
-            }).catch(error => {
-                console.log(error);
-            });
+  const fetchData = async () => {
+    await axios.get('http://localhost:5000/getTeamSpaceTotalBudget', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
+      .then(response => {
+        setTotalBudget(response.data.data.totalBudget);
+      }).catch(error => {
+        console.log(error);
+      });
+    await axios.get('http://localhost:5000/getTeamSpaceTotalAmountUsed', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
+      .then(response => {
+        setTotalSpent(response.data.data.totalAmountUsed);
+      }).catch(error => {
+        console.log(error);
+      });
+    await axios.get('http://localhost:5000/getAllSpendingCategories', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
+      .then(response => {
+        setSpendingCategories(response.data.data);
+        let currLargest = response.data.data[0];
+        for (let i = 0; i < response.data.data.length; i++) {
+          if (response.data.data[i].amountUsed > currLargest.amountUsed) {
+            currLargest = response.data.data[i];
+          }
+        }
+        setLargestSpendingCategory(currLargest);
+      }).catch(error => {
+        console.log(error);
+      });
+    await axios.get('http://localhost:5000/getRecentTransactions', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
+      .then(response => {
+        setTransactions(response.data.data.recentTransactions);
+      }).catch(error => {
+        console.log(error);
+      });
 
-        await axios.get('http://localhost:5000/getTeamSpaceByID', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
-            .then(response => {
-                setTeamSpaceName(response.data.data.teamSpaceName);
-                if (response.data.data.teamSpaceLeaderUserID === window.localStorage.getItem("userID")) {
-                    setIsLeader(true);
-                } else {
-                    setIsLeader(false);
-                }
-            }).catch(error => {
-                console.log(error);
-            });
-        await axios.get('http://localhost:5000/getTeamSpaceByUserID', { params: { "userID": window.localStorage.getItem("userID") } })
-            .then(response => {
-                if (response.data.data === null) {
-                    window.localStorage.removeItem("teamSpaceID")
-                    window.location.replace('/register')
-              }
-            }).catch(error => {
-                console.log(error)
-            });
-    };
+    await axios.get('http://localhost:5000/getTeamSpaceByID', { params: { "teamSpaceID": window.localStorage.getItem("teamSpaceID") } })
+      .then(response => {
+        setTeamSpaceName(response.data.data.teamSpaceName);
+        if (response.data.data.teamSpaceLeaderUserID === window.localStorage.getItem("userID")) {
+          setIsLeader(true);
+        } else {
+          setIsLeader(false);
+        }
+      }).catch(error => {
+        console.log(error);
+      });
+    await axios.get('http://localhost:5000/getTeamSpaceByUserID', { params: { "userID": window.localStorage.getItem("userID") } })
+      .then(response => {
+        if (response.data.data === null) {
+          window.localStorage.removeItem("teamSpaceID")
+          window.location.replace('/register')
+        }
+      }).catch(error => {
+        console.log(error)
+      });
+  };
 
-    const openCreateCategoryModal = () => {
-      setIsCreateCategoryModalOpen(true);
-    };
+  const openCreateCategoryModal = () => {
+    setIsCreateCategoryModalOpen(true);
+  };
 
-    const closeCreateCategoryModal = () => {
-      setIsCreateCategoryModalOpen(false);
-    };
-
-    const openAddTransactionModal = () => {
-      setIsAddTransactionModalOpen(true);
-    };
-
-    const closeAddTransactionModal = () => {
-      setIsAddTransactionModalOpen(false);
-    };
+  const closeCreateCategoryModal = () => {
+    setIsCreateCategoryModalOpen(false);
+  };
 
 
-    return (
-      <div className="home flex flex-col items-center justify-center">
-        <div className="flex justify-between items-center">
-          <div>
-            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Hello {window.localStorage.getItem("username")}</h1>
-            <h2 className="text-l text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Team Space: {teamSpaceName}</h2>
-          </div>
-        </div>
+  return (
+    <div className="home flex flex-col items-center justify-center">
 
-        <div className="flex">
-          <InfoCard title="Total Budget" value={totalBudget} />
-          <InfoCard title="Amount Used" value={totalSpent} />
-        </div>
-        <div className="flex flex-wrap items-center justify-center mt-5 mb-5">
-          {spendingCategories.map((category, index) => (
-            <CategoryCard key={index} category={category} />
-          ))}
+      <div className="flex">
+        {/* <InfoCard title="Total Budget" value={totalBudget} />
+        <InfoCard title="Amount Used" value={totalSpent} /> */}
+        <Jumbotron username={window.localStorage.getItem("username")} transactions={transactions} spendingCategory={largestSpendingCategory} teamSpaceName={teamSpaceName} />
+      </div>
+      <div className="flex flex-wrap items-center justify-center mt-5 mb-5" id="categories">
+        {spendingCategories.map((category, index) => (
+          <CategoryCard key={index} category={category} />
+        ))}
 
         {isLeader && (
-            <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ml-5 mr-5 h-36 w-36 flex items-center justify-center cursor-pointer" onClick={openCreateCategoryModal}>
-                <VscDiffAdded  size={80}/>
-            </div>
+          <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ml-5 mr-5 h-36 w-36 flex items-center justify-center cursor-pointer" onClick={openCreateCategoryModal}>
+            <VscDiffAdded size={80} />
+          </div>
         )}
-        </div>
-
-        {isCreateCategoryModalOpen && (
-          <CreateCategoryModal
-            onClose={closeCreateCategoryModal}
-          />
-        )}
-
-        <LatestTransactionsCard transactions={transactions} />
-        <button
-            className="bg-primary-500 hover:bg-primary-700 focus:ring-4 px-5 py-2.5 rounded-lg text-sm text-white font-medium text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            onClick={openAddTransactionModal}
-          >
-            Add Transaction
-          </button>
-         {isAddTransactionModalOpen && (
-          <CreateTransactionModal 
-            onClose={closeAddTransactionModal}
-            spendingCategories={spendingCategories}
-          />
-         )}
       </div>
-    );
+
+      {isCreateCategoryModalOpen && (
+        <CreateCategoryModal
+          onClose={closeCreateCategoryModal}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Home;
