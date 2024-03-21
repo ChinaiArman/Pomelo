074d29c116558f44c0b8d8842e011a7daf3e3209
ChinaiArman <chinaiarman@gmail.com>
2024-03-21 11:45:38 -0700

    Update nav bar logo

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Dropdown } from "flowbite-react";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import logo from "../assets/logo_full.png";
 
 const NavBar = () => {
 
@@ -40,8 +41,7 @@ const NavBar = () => {
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MyApp</span>
+          <img src={logo} alt="Pomelo Logo" className="mt-2 h-10" />
         </a>
         <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
