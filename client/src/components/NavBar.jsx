d598212022f62826eafceb9668540df3d0846855
Natalie Yu <nyu17@my.bcit.ca>
2024-03-21 12:35:51 -0700

    Add key to child li elements

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -45,16 +45,16 @@ const NavBar = () => {
         </a>
         <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            {navItems.slice(0, -1).map((item) => {
+            {navItems.slice(0, -1).map((item, index) => {
               if (item.link === window.location.pathname) {
                 return (
-                  <li>
+                  <li key={index}>
                     <a href={item.link} className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">{item.text}</a>
                   </li>
                 )
               } else {
                 return (
-                  <li>
+                  <li key={index}>
                     <a href={item.link} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{item.text}</a>
                   </li>
                 )
