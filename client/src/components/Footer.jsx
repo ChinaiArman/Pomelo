838dac9ebbb54f8aed35b8f3564306811f5002e3
ChinaiArman <chinaiarman@gmail.com>
2024-03-21 23:07:56 -0700

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,10 @@
 const Footer = () => {
     return (
-        <div className="footer">
-            <p>Footer</p>
+        <div className="flex flex-wrap justify-end items-center">
+            <div>
+                <p className="text-gray-500 text-sm mr-5">© COMP 3962 Pomelo</p>
+                <p className="text-gray-500 text-sm mr-5 mb-3">Arman Chinai, Lex Wong, Natalie Yu</p>
+            </div>
         </div>
     );
 }
