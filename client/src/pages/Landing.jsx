52d20a93916507811caec98082413b0f4f816e26
Natalie Yu <nyu17@my.bcit.ca>
2024-03-22 11:26:39 -0700

    Make landing page bigger

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -11,17 +11,17 @@ const Landing = () => {
     }
 
     return (
-        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 flex justify-center items-center flex-col h-screen">
-            <div className="text-lg font-normal text-gray-500 dark:text-gray-400 mb">
-                <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">Welcome to</h1>
+        <div className="flex justify-center items-center flex-col h-screen w-screen mx-auto">
+            <div>
+                <h1 className="text-gray-900 dark:text-white text-8xl font-extrabold mb-2">Welcome to</h1>
             </div>
             <div>
-                <img src={logo} className="inline h-200px" style={{ height: "200px" }}>
+                <img src={logo} className="inline" style={{ height: "350px" }}>
                 </img>
             </div>
-            <div className="flex space-x-4">
+            <div className="flex space-x-8">
                 <button
-                    className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-theme-mantis hover:bg-theme-mantisdark focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+                    className="inline-flex justify-center items-center py-2.5 px-8 text-base font-medium text-center text-white rounded-lg bg-theme-mantis hover:bg-theme-mantisdark focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
                     onClick={handleLogin}
                 >
                     Login
@@ -30,7 +30,7 @@ const Landing = () => {
                     </svg>
                 </button>
                 <button
-                    className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-theme-mantis hover:bg-theme-mantisdark focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+                    className="inline-flex justify-center items-center py-2.5 px-8 text-base font-medium text-center text-white rounded-lg bg-theme-mantis hover:bg-theme-mantisdark focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
                     onClick={handleSignUp}
                 >
                     Sign Up
