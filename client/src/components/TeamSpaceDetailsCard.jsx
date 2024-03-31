1b833fcc919cf7efdf3a41858bf883663d09ef67
Natalie Yu <nyu17@my.bcit.ca>
2024-03-31 03:03:20 -0700

    Add Team Space Join code to team space settings

--- client/src/components/TeamSpaceDetailsCard.jsx
+++ client/src/components/TeamSpaceDetailsCard.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { CiEdit } from "react-icons/ci";
 import EditTeamSpaceModal from "./EditTeamSpaceModal";
 import { currencyFormatter } from '../utils';
@@ -6,6 +7,21 @@ import logo from "../assets/logo_icon.png";
 
 const TeamSpaceDetailsCard = ({teamSpaceName, totalBudget}) => {
     const [isEditTeamSpaceModalOpen, setIsEditTeamSpaceModalOpen] = useState(false);
+    const [joinCode, setJoinCode] = useState('');
+
+    useEffect(() => {
+      getJoinCode();
+    })
+
+    const getJoinCode = async function() {
+      await axios.get('http://localhost:5000/getJoinCode', {
+        params: {"teamSpaceID": window.localStorage.getItem("teamSpaceID")}
+      }).then(response => {
+        setJoinCode(response.data.data);
+      }).catch(error => {
+        console.log(error);
+      })
+    }
 
     const openEditTeamSpaceModal = () => {
         setIsEditTeamSpaceModalOpen(true);
@@ -23,6 +39,15 @@ const TeamSpaceDetailsCard = ({teamSpaceName, totalBudget}) => {
             <img src={logo} alt="Pomelo Logo" className="mt-2 h-20" />
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{teamSpaceName}</h5>
             <h4 className="mb-1 text-l font-medium text-gray-600 dark:text-white">Total Budget: {currencyFormatter.format(totalBudget)}</h4>
+            <span className="flex items-center my-2">
+              <h4 className="mb-1 mr-2 text-base font-medium text-gray-600 dark:text-white">Team Join Code: </h4>
+              <input
+                type="text"
+                readOnly={true}
+                defaultValue={joinCode}
+                className="w-24 border border-gray-300 rounded-lg px-2 py-2 text-xs bg-theme-oldlace focus:border-theme-mantis focus:ring-theme-mantisdark"
+              />
+            </span>
             <div className="flex mt-4 md:mt-6">
                 <button 
                     className="bg-theme-mantis hover:bg-theme-mantisdark focus:ring-4 px-5 py-2.5 rounded-lg text-sm text-white font-medium text-center mr-2"
