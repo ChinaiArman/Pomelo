3dd9c0e00fb616c2653db83413243ac24b3c6662
Natalie Yu <nyu17@my.bcit.ca>
2024-03-31 03:03:44 -0700

    Remove unused div

--- client/src/components/TeamSpaceDetailsCard.jsx
+++ client/src/components/TeamSpaceDetailsCard.jsx
@@ -33,8 +33,6 @@ const TeamSpaceDetailsCard = ({teamSpaceName, totalBudget}) => {
     
     return (
     <div className="w-full max-w-sm bg-theme-cornsilk border border-theme-cornsilk rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <div className="flex justify-end px-4 pt-4">
-        </div>
         <div className="flex flex-col items-center pb-10">
             <img src={logo} alt="Pomelo Logo" className="mt-2 h-20" />
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{teamSpaceName}</h5>
