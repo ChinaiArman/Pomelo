b0023f9bca611db036d904259451c5fa7e79ad34
ChinaiArman <chinaiarman@gmail.com>
2024-04-05 14:43:34 -0700

    Add client build

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,7 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export const plugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+};
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
-  }
+    port: process.env.PORT || 3001,
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss('./tailwind.config.js'),
+        autoprefixer(),
+      ],
+    },
+  },
 })
