6fc1da9a574458c8deb8efef17dd4b302e5ba2d7
ChinaiArman <chinaiarman@gmail.com>
2024-06-04 19:50:14 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -219,15 +219,9 @@ app.post("/editTransaction", async (req, res) => {
 
 // Cognito
 app.post("/login", async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  // res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   if (req.method === 'OPTIONS') {
     res.status(200).end()
     return
